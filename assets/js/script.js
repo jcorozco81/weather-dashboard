1612c0903b743a7c80b85ec787776f61fcb125da
jcorozco81 <jcorozco@gmail.com>
2021-06-10 03:09:35 -0400

    added icons and links to searched cities feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,13 @@
 // Current by City ID: 'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=9a238e861db9760ef96c88c9e9a32044'
 // Geocoding: 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=9a238e861db9760ef96c88c9e9a32044' - City Name and Lat and Long
 
-
-// Check and render Saved Locations
+// Variables initial rendering
 var targerID = 99;
 var cityData;
 var tosave=[];
 renderLocations();
 
+// render Saved Locations fuction
 function renderLocations(){
 var savedLocations = JSON.parse(localStorage.getItem("saved-locations"));
     if (savedLocations !== null)
@@ -26,6 +26,7 @@ var savedLocations = JSON.parse(localStorage.getItem("saved-locations"));
         // console.log(tosave);
     }
 }
+
 
 //Search Button Fuction
 var searchButton=$("#search-location");   
@@ -39,11 +40,7 @@ searchButton.on('click', function (event) {
     }
     else{
 
-        console.log(locationText);
-        tosave.splice(4, 1);
-        tosave.splice(0, 0, locationText);
-        localStorage.setItem("saved-locations", JSON.stringify(tosave));
-        renderLocations();
+
         $('.input').val("");
         $("#alert-empty").text("");
       processLocation(locationText);
@@ -53,6 +50,18 @@ searchButton.on('click', function (event) {
 })
 
 
+//Function to save data in local storage
+function toLocalstorage(locText){
+
+  console.log(locText);
+  tosave.splice(4, 1);
+  tosave.splice(0, 0, locText);
+  localStorage.setItem("saved-locations", JSON.stringify(tosave));
+  renderLocations();
+
+}
+
+
 
 
 
@@ -60,6 +69,7 @@ searchButton.on('click', function (event) {
   var requestUrl2;
   var requestUrl3;
 
+//Function to process selected city
   function processLocation(cityText){
 requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+cityText+'&limit=5&appid=9a238e861db9760ef96c88c9e9a32044';
 console.log(requestUrl);
@@ -71,7 +81,7 @@ console.log(requestUrl);
     .then(function (data) {
         console.log(data);
         cityData=data;
-      if(data.length>0){
+      if(data.length>=1){
             console.log("Multiple Locations" + data.length);
             $('.modal').addClass('is-active');
             selectionList(data);
@@ -80,7 +90,7 @@ console.log(requestUrl);
       }
 
 
-
+//Function to get weather data from API
 function getWeather(latRec, lonRec){
 console.log(latRec);
 console.log(lonRec);
@@ -93,45 +103,52 @@ fetch(requestUrl2)
   .then(function (data) {
     //   console.log(data);
       console.log("Current Date:" + data.current.dt);
+      $(".icon").css("display", "block");
+      // $("main .notification").css("display", "default");
+      // $("main .is-dark").css("display", "default");
+      $("main").css("display", "block");
+
       var currentDate=dateConvert(data.current.dt);
       $("#current-date").text(currentDate);
+      $("#current-icon").attr("src", "https://www.openweathermap.org/img/wn/"+ data.current.weather[0].icon + "@2x.png");
       console.log("Current Temp:" + data.current.temp+" °F");
-      $("#current-temp").text(data.current.temp+" °F");
+      $("#current-temp").text("Temperature: " + data.current.temp+" °F");
       console.log("Current Humidity:" + data.current.humidity+" %");
-      $("#current-humidity").text(data.current.humidity+" %");
+      $("#current-humidity").text("Humidity: " + data.current.humidity+" %");
       console.log("Current Wind Speed:" + data.current.wind_speed+" mph");
-      $("#current-windspeed").text(data.current.wind_speed+" mph");
+      $("#current-windspeed").text("Wind Speed: " + data.current.wind_speed+" mph");
       console.log("Current UV Index:" + data.current.uvi);
  
       $('#current-uvi').removeClass();
       // var uvIndextxt;
       if(data.current.uvi<3){
-        $('#current-uvi').addClass('tag is-success');
+        $('#current-uvi').addClass('tag is-success content is-medium');
       }
       else if(data.current.uvi<8 && data.current.uvi>=3){
-        $('#current-uvi').addClass('tag is-warning');
+        $('#current-uvi').addClass('tag is-warning content is-medium');
       }
       else if(data.current.uvi>=8){
-        $('#current-uvi').addClass('tag is-danger');
+        $('#current-uvi').addClass('tag is-danger content is-medium');
       }
-      $("#current-uvi").text(data.current.uvi)
+      $("#current-uvi").text("UV Index: "+ data.current.uvi);
 
-for (var i=1; i<6; i++){
+for (var i=0; i<6; i++){
+  $("#date-"+i+"-icon").attr("src", "https://www.openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon + "@2x.png");
     var futureDate=dateConvert(data.daily[i].dt);
     console.log(data.daily[i].dt);
     $("#date-"+i).text(futureDate);
     console.log(data.daily[i].temp.day);
-    $("#date-"+i+"-temp").text(data.daily[i].temp.day+" °F");
+    $("#date-"+i+"-temp").text("Temperature: " +data.daily[i].temp.day+" °F");
     console.log(data.daily[i].wind_speed);
-    $("#date-"+i+"-humidity").text(data.daily[i].humidity+" %");
+    $("#date-"+i+"-humidity").text("Humidity: " + data.daily[i].humidity+" %");
     console.log(data.daily[i].humidity);
-    $("#date-"+i+"-windspeed").text(data.daily[i].wind_speed+"  mph");
+    $("#date-"+i+"-windspeed").text("Wind Speed: " + data.daily[i].wind_speed+"  mph");
 }
 });
 }
 
 
-
+//Function to get city ID
 function getCityID(){
 requestUrl3 = 'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=9a238e861db9760ef96c88c9e9a32044';
 fetch(requestUrl3)
@@ -145,6 +162,7 @@ fetch(requestUrl3)
 
 }
 
+//Function to convert date
 //From: https://www.geeksforgeeks.org/how-to-convert-date-to-another-timezone-in-javascript/
 function dateConvert(dateRaw){
     var newdate = new Date(dateRaw*1000).toDateString();
@@ -156,7 +174,7 @@ function dateConvert(dateRaw){
 
 
 
-
+// Show options in Modal
 function selectionList(data){
   for(var i=0; i<data.length; i++){
     //  $("#city"+[i]).text(data[i].name+", "+data[i].country+", ");
@@ -169,16 +187,10 @@ function selectionList(data){
       $("#city-"+[i]).text(data[i].name+" - "+data[i].country);
         }
    }
-
-
-  
-
-
-
 }
 
-// Modal Button
 
+// Modal Fuctions
 var modalDelete=$(".delete");   
 modalDelete.on('click', function (event) {
   event.preventDefault();
@@ -196,13 +208,10 @@ function cleanModal(){
       $("#city-"+[i]).text("");
 }}
 
-
-
 // var modalCancel=$("#cancel-button");   
 // modalCancel.on('click', function (event) {
 //   event.preventDefault();
 //   $('.modal').removeClass('is-active');
-
 // })
 
 
@@ -226,6 +235,7 @@ function sendCoordinates(){
       var targetClicked = event.target;
       targerID=targetClicked.id;
       selectedCity = event.target.innerText;
+      toLocalstorage(selectedCity);
       console.log("Target Clicked: " + selectedCity);
       $('#current-city').text(selectedCity);
       targerID=targerID.charAt(5);
@@ -237,3 +247,11 @@ function sendCoordinates(){
      })
 
 
+     var clickLink=$(".saved-link");   
+     clickLink.on('click', function (event) {
+       event.preventDefault();
+       var targetClicked = event.target.innerText;
+       targetClicked = targetClicked.split("-");
+       targetClicked[0] = targetClicked[0].trim();
+       processLocation(targetClicked[0]);
+     })
